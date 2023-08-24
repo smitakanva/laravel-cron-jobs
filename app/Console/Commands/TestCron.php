000d24728478f6731b8a18685235607de94cb198
smitakanva <smita.mind2web@gmail.com>
2023-08-24 18:20:25 +0530

    first commit

---
+++ app/Console/Commands/TestCron.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+  
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Log::info("Successfully, cron is running");
+        // Inside this handle() method, you can add any type of your job task as Sending an email, 
+        // Data insert into database, taking data dumps etc.
+    }
+}
